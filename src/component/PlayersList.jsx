7ecab910bd1bd164e662962a51fddec1f93f13dc
Rawane <133503500+RAWANE18@users.noreply.github.com>
2024-08-08 19:40:35 +0100

    commited

--- src/component/PlayersList.jsx
+++ src/component/PlayersList.jsx
@@ -5,6 +5,7 @@ import Player from './Player';
 export default function PlayersList() {
   return (
     <>
+      {/* mapping through all the elements in the array of players */}
        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
             {players.map((player) => (
                 <Player key={player.jerseyNumber} {...player} />
